41d50a9eff34c99f6aae95b49c611252d0753886
charmi shah <charmishah241996@gmail.com>
2019-07-29 18:26:08 +0545

    secound commit only one person data dispplay data from parent to child2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
+
 export class AppComponent {
-  name = 'Charmi';
-  designation = 'Front-end developer';
-  email = 'Charmi@gmail.com';
-  contact = '8200772753';
+
+
+  person={
+  name : 'Charmi',
+  designation : 'Front-end developer',
+  email : 'Charmi@gmail.com',
+  contact : '8200772753'
+}
+// person2={
+//   name : 'Charvi',
+//   designation : 'Front-end developer',
+//   email : 'Charmi@gmail.com',
+//   contact : '8200772753'
+// }
+
 }
--- src/app/server2/server2.component.ts
+++ src/app/server2/server2.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-server2',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Server2Component implements OnInit {
 
+  @Input() employee:any;
+  // @Input() employee2:any;
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
